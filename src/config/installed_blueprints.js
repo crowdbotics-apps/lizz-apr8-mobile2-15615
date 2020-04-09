0974f0d13494d056b52457b364d000419d1f09aa
Crowdbotics <team@crowdbotics.com>
2020-04-09 17:35:36 +0000

    add package blueprint-react-native-contacts-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Contacts27357', human_name: 'Contacts', access_route: 'Contacts27357'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
 { name: 'Tutorial', human_name: 'Tutorial', access_route: 'Tutorial', icon: 'question-circle'},
 { name: 'Maps', human_name: 'Maps', access_route: 'Maps', icon: 'map'},
